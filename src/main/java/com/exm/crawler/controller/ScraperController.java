565a097fdad6616b646209982afa5afa1daede0c
hozaifamoaj <hozaifa.moaj@gmail.com>
2023-07-31 16:46:00 +0600

    removed elastic config and unwanted code

--- src/main/java/com/exm/crawler/controller/ScraperController.java
+++ src/main/java/com/exm/crawler/controller/ScraperController.java
@@ -2,23 +2,13 @@ package com.exm.crawler.controller;
 
 import com.exm.crawler.model.ResponseDTO;
 import com.exm.crawler.service.ScraperService;
-import org.jsoup.Jsoup;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
-import java.util.concurrent.TimeUnit;
-import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping(path = "/api/crawler")
